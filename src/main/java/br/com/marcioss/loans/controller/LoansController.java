fcfb5080928b4caf3cc93d0ec89364f1560ca1cd
marcio silva <marcioadsworks@gmail.com>
2023-05-04 21:24:10 -0300

    get properties from config server

--- src/main/java/br/com/marcioss/loans/controller/LoansController.java
+++ src/main/java/br/com/marcioss/loans/controller/LoansController.java
@@ -1,9 +1,15 @@
 package br.com.marcioss.loans.controller;
 
+import br.com.marcioss.loans.config.LoansServiceConfig;
 import br.com.marcioss.loans.model.Customer;
 import br.com.marcioss.loans.model.Loans;
+import br.com.marcioss.loans.model.Properties;
 import br.com.marcioss.loans.repository.LoansRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,8 +22,19 @@ public class LoansController {
     @Autowired
     private LoansRepository loansRepository;
 
+    @Autowired
+    private LoansServiceConfig serviceConfig;
+
     @PostMapping("/myLoans")
-    public List<Loans> getLoansDetails(@RequestBody Customer customer){
+    public List<Loans> getLoansDetails(@RequestBody Customer customer) {
         return loansRepository.findByCustomerIdOrderByStartDtDesc(customer.getCustomerId());
+    }
+
+    @GetMapping("/loans/properties")
+    public String loansProperties() throws JsonProcessingException {
+        ObjectWriter objectWriter = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        Properties properties = new Properties(serviceConfig.getMsg(), serviceConfig.getBuildVersion(),
+                serviceConfig.getMailDetails(), serviceConfig.getActiveBranches());
+        return objectWriter.writeValueAsString(properties);
     }
 }
